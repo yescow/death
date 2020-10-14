b51492fa08def926c66716c33bc7c85b2d452318
yescow <fBHgV2WXgHcKUusy9hNHTuxJGx9b8lVcZ1S93CM@protonmail.com>
2020-10-14 13:42:21 -0400

    initial commit

---
+++ death.py
@@ -0,0 +1,40 @@
+class ListenForTwoMinutes:
+  def call_human_needs_(self):
+    return
+  def click_wrong_item(self):
+    return
+  def self_self(self):
+    return
+  def drink(self):
+    return
+  def insert_bottles_into_backpack(self):
+    return
+  def smoke_fake_cig(self):
+    return
+  def Forgive(self):
+    return
+  def league_of_legends(self):
+    return
+  def troll_phone_until_since_representative_(self):
+    return
+  def re_call(self):
+    return
+  def begin_to_feel_hopeless(self):
+    return
+  def take_zero_percent_chance(self):
+    return
+  def continue_class_writing(self):
+    return
+  def am_i_retarded(self):
+    return
+  def imogen_heal(self):
+    return
+  def get_convinced_by_artist(self):
+    return
+  def zoom_out_window(self):
+    return
+  def extinct_owen_cook(self):
+    return
+  def at_least_be_on_whitechalk(self):
+    return
+  
